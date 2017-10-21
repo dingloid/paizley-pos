df1391f99502f1c245374e207ce4060c1e95470b
Unknown <winston.suryana@gmail.com>
2017-10-21 12:27:56 -0500

    added item list activity

--- app/src/main/java/grayteam/paizley_pos/MainActivity.java
+++ app/src/main/java/grayteam/paizley_pos/MainActivity.java
@@ -1,5 +1,6 @@
 package grayteam.paizley_pos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i = new Intent(this, ItemListActivity.class);
+        startActivity(i);
     }
 }
